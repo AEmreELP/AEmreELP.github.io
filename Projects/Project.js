d93895f4bbc4b71ea36a9d34463e911086887d19
A.Emre ELP <a.emreelp@gmail.com>
2023-05-19 18:57:29 +0300

    Contact Changed

---
+++ Projects/Project.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+
+    $("#CommentButton ").click(function (evt){
+
+        $("#ContactId ").fadeIn()
+        $("#MemberJoin ").fadeOut()
+        
+
+    })
+
+
+
+})//ready end
