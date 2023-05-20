db71c243dcba61a647799a33a158641247fdfbaa
A.Emre ELP <a.emreelp@gmail.com>
2023-05-20 17:05:03 +0300

    Color changing added

--- Projects/Project.js
+++ Projects/Project.js
@@ -1,7 +1,19 @@
 var universities = ['AYBU','KU','TOBB']
 var faculties = ['MEDICINE','EDUCATION','ENGINEERING','FORESTRY']
 var departments = ['MEDICINE','TEACHER TRAINING IN FRENCH','TEACHER TRAINING IN ENGLISH','TEACHER TRAINING IN GERMAN','COMPUTER ENGINEERING','ELECTRICAL-ELECTRONIC ENGINEERING','INDUSTRIAL ENGINEERING','GEOPHYSICAL ENGINEERING','CIVIL ENGINEERING','FOREST ENGINEERING ','FOREST INDUSTRIES ENGINEERING ']
-var classes = ['Preparatory class(English)','1.Class','2.Class','3.Class','4.Class','5.Class','6.Class','Graduated']
+var classes = ['Preparatory class(English)','1.Class','2.Class','3.Class','4.Class','5.Class','6.Class']
+
+var safeColors = ['00','11','22','33','44','55','66','77','88','99','aa','bb','cc','dd','ee','ff'];
+var rand = function() {
+    return Math.floor(Math.random()*16);
+};
+var randomColor = function() {
+    var r = safeColors[rand()];
+    var g = safeColors[rand()];
+    var b = safeColors[rand()];
+    return "#"+r+g+b;
+};
+
 $(document).ready(function () {
     //datapicker
     $("#birthday").datepicker();
@@ -41,6 +53,20 @@ $(document).ready(function () {
 
     })
 
+    //alert(),each() and .css() used to change background color
+    $('img[src="assets/images/logo.png"]').click(function() {
+        alert("Content color changed");
+        $('.content').each(function() {
+            $(this).css('background',randomColor());
+        });
+    })
+
+    $('h3').click(function() {
+        alert("Pages all parts color changed");
+        $('div').each(function() {
+            $(this).css('background',randomColor());
+        });
+    })
 
 
 })//ready end
