67dd561b986ccb2b3eed74533455d3a45033af88
A.Emre ELP <a.emreelp@gmail.com>
2023-05-30 19:47:11 +0300

    sliderError

--- Activity10_Slider/slider.js
+++ Activity10_Slider/slider.js
@@ -1,16 +1,24 @@
-$(document).ready(function () {
-		$("#slider").bxSlider({
-			auto: true,
-			slideWidth: 250,
-			slideMargin: 10,
-			randomStart: true,
-			speed: 300,
-			moveSlides: 1,
-			pager: true,
-			pagerType: "short",
-			touchEnabled: false
-		});
-	
+/*$(document).ready(function () {
+	$("#slider").bxSlider({
+		auto: true,
+		slideWidth: 250,
+		slideMargin: 10,
+		randomStart: true,
+		speed: 300,
+		moveSlides: 1,
+		pager: true,
+		pagerType: "short",
+		touchEnabled: false
+	});
+
 });
-
-
+*/
+$(document).ready(function () {
+	$("#slider").bxSlider({
+		auto: true,
+		minSlides: 2,
+		maxSlides: 2,
+		slideWidth: 250,
+		slideMargin: 10
+	});
+});
