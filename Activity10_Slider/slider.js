bad1e2ee27cb8c430684625433b890a9428d4291
A.Emre ELP <a.emreelp@gmail.com>
2023-05-30 15:51:58 +0300

    Slider is finished

--- Activity10_Slider/slider.js
+++ Activity10_Slider/slider.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
         	$("#slider").bxSlider({
 			  auto: true,
-			  minSlides: 2,
-			  maxSlides: 2,
 			  slideWidth: 250,
 			  slideMargin: 10,
 			  randomStart: true,
