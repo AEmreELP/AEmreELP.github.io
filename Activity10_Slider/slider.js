b1d849ecf1ffde5bfa63d3f9269f07ab8506b805
A.Emre ELP <a.emreelp@gmail.com>
2023-05-30 19:14:10 +0300

    sliderla uğraşıyorum

--- Activity10_Slider/slider.js
+++ Activity10_Slider/slider.js
@@ -1,13 +1,17 @@
-$(document).ready(function() {
-        	$("#slider").bxSlider({
-			  auto: true,
-			  slideWidth: 250,
-			  slideMargin: 10,
-			  randomStart: true,
-			  speed: 300,
-			  moveSlides: 1,
-			  pager: true,
-			  pagerType: "short",
-			  touchEnabled: false
-			});
-    	});
+$(document).ready(function () {
+	require(['jquery', 'jquery/ui', 'NameSpace_Module/web/js/your_module'], function ($, bxSlider) {
+		$("#slider").bxSlider({
+			auto: true,
+			slideWidth: 250,
+			slideMargin: 10,
+			randomStart: true,
+			speed: 300,
+			moveSlides: 1,
+			pager: true,
+			pagerType: "short",
+			touchEnabled: false
+		});
+	})
+});
+
+
