07d54e6e072ddeb2ca52ff388eed82d064451aa2
A.Emre ELP <a.emreelp@gmail.com>
2023-05-30 15:22:14 +0300

    Files Added

---
+++ Activity10_Slider/slider.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+        	$("#slider").bxSlider({
+			  auto: true,
+			  minSlides: 2,
+			  maxSlides: 2,
+			  slideWidth: 250,
+			  slideMargin: 10,
+			  randomStart: true,
+			  speed: 300,
+			  moveSlides: 1,
+			  pager: true,
+			  pagerType: "short"
+			});
+    	});
